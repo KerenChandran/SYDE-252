7e49a546f2cff0b459d7dced1687593de6bb17a2
Siddhanth Unnithan <sidunnithan@gmail.com>
2015-11-08 12:38:22 -0500

    computed ak over entire sample

--- Assignment 1/ak_script.m
+++ Assignment 1/ak_script.m
@@ -9,7 +9,7 @@ t = 0:0.1:3
 x = 1+cos(2*pi*t)/4+cos(2*pi*t*2)/2 + cos(2*pi*t*3)/3;
 
 %specified fundamental frequency of x(t)
-wo = 1
+wo = 2*pi
 
 %specified k value
 k = 1
@@ -20,5 +20,18 @@ comp = exp(-j*wo*k*t);
 %calculated period 
 T = 2*pi/wo
 
+%return index of element representing time after 1 period 
+ix_last = find(t==t(1) + T)
+
+prod = []
+
+for i:length(x):
+    prod = [prod x(i)*comp(i)]
+end
+
 %compute ak 
-ak = (1/T)*trapz()
+ak = (1/T)*trapz(prod, t)
+
+ak = real(ak)
+
+
