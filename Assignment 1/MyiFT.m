82957d52a92f91689bc6e75fdf514d76b9ed66a6
Keren Chandran <keren.chandran@gmail.com>
2015-11-08 16:53:27 -0500

    Modified MyiFT

--- Assignment 1/MyiFT.m
+++ Assignment 1/MyiFT.m
@@ -1,36 +1,23 @@
 function z = MyiFT(wt,w,t)
-    %{
-            Parameters
-            ----------
-            x - signal; array representation
-            t - time sample array
-            k - particular coefficient to be determined
-            wo - fundamental frequency of signal
-        %}
+%{
+    Parameters
+    ----------
+    wt - signal; array representation
+    t - time sample array
+    wo - fundamental frequency of signawt
+    %}
 
-    % time-domain sample array - 100ms samples between 1 and 3 seconds
-    t = 0:0.1:3
+comp = exp(j*w*t)
 
-    %arbitrary function x(t)
-    x = 1+cos(2*pi*t)/4+cos(2*pi*t*2)/2 + cos(2*pi*t*3)/3;
+%product vector used for integral
+prod = []
 
-    %specified fundamental frequency of x(t)
-    wo = 2*pi
-
-    comp = exp(-j*wo*k*t)
-    %obtain index of time sample corresponding to element after one period
-    ix_last = find(t==t(1)+t)
-
-    %product vector used for integral
-    prod = []
-
-    for i=1 : length(x)
-        prod = [prod x(i)*comp(i)]
-    end
-
-    % x(t) = 1/2pi * integral from -inf to +inf of X(w)e^jwot dw
-    
-    z = (1/(2*pi)) * trapz(prod(1:ix_last), t(1:ix_last))
-
+for i = 1 : length(wt)
+prod = [prod wt(i) * comp(i)]
 end
 
+% x(t) = 1/2pi * integral from -inf to +inf of X(w)e^jwot dw
+
+z = (1/(2*pi)) * trapz(prod, t)
+
+end
