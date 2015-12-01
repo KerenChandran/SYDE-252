a21e9bd29e5dae2ed7a4b399780122602a977aa0
Siddhanth Unnithan <sidunnithan@gmail.com>
2015-11-30 23:13:15 -0500

    myft change from prev a. - needs fix

--- Assignment 1/MyFT.m
+++ Assignment 1/MyFT.m
@@ -2,6 +2,17 @@ function Xw = MyFT(Xt, w, t)
     %frequency representation placeholder
    Xw = []
 
+   %{
+    Arbitrary bound was chosen for the loop representing different values of w.
+    Using different values of w, one can obtain different frequency values.
+   %}
+
+   %using Fourier Transform Analysis equation 
+   for w = 0 : t(end):
+        Xw = trapz(Xt*exp(-j*w*t), t)
+   end 
+
+   %{
    %using Fourier Transform envelope property
    for k = -Inf:Inf
        wo = w/k
@@ -10,4 +21,5 @@ function Xw = MyFT(Xt, w, t)
 
        Xw = [Xw MyFSAnalysis(Xt, t, k, wo)*To]
    end
+   %}
 end
