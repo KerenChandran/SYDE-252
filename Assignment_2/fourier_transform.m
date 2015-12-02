177b3a27de877c9f2a20bdafedcc3f8cda574f35
Keren Chandran <keren.chandran@gmail.com>
2015-12-01 20:47:47 -0500

    Fixed Fourier Transform

--- Assignment_2/fourier_transform.m
+++ Assignment_2/fourier_transform.m
@@ -2,7 +2,7 @@ function Xw = fourier_transform(Xt, w, t)
     Xw = [];
 
     for i = w
-        y = exp(-j*i*t)
-        Xw = [Xw norm(trapz(t, Xt * y.'))]
+        Xw = [Xw real(Xt * exp(-j*i*t)' * (1/16000))];
     end
+              
 end
