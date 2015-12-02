25a1f2216a3ffc04daa27c7954850978e11f596f
Keren Chandran <keren.chandran@gmail.com>
2015-12-02 09:33:04 -0500

    Created a separate file for sampling frequency. Also created a new console_script file that contains how to take a sample of our signal and pass it into the fourier transform. PS: Graph axis is recorded in frequency now.

---
+++ Assignment_2/sample.m
@@ -0,0 +1,16 @@
+function s = sample(acqData)
+    beg = 1
+    left_speaker = acqData(:,1)
+    lower_bound = beg
+
+    %iterate through all points and find upper and lower bound of dial tone
+    for pt = beg:length(left_speaker)
+        if left_speaker(pt) > 0.05 && lower_bound == beg
+            lower_bound = pt
+            break
+        end
+    end
+
+    t = left_speaker(lower_bound:lower_bound + 3000)
+
+end
